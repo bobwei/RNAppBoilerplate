05f99adce7801bd52f1a3fb4fea7553e6c9846a5
Bob Wei <bob.wei@linecorp.com>
2018-05-17 21:40:35 +0800

    Setup boilerplate

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
 
-AppRegistry.registerComponent('RNAppBoilerplate', () => App);
+AppRegistry.registerComponent('App', () => App);
+
+const { YellowBox } = require('react-native');
+
+YellowBox.ignoreWarnings([
+  'Warning: isMounted(...) is deprecated',
+  'Module RCTImageLoader requires',
+  'Class RCTxxModule was not exported',
+]);
